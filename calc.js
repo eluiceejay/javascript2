7318ab085e062d6e13a46dbfc810564223f2157a
eluiceejay <efjioke@gmail.com>
2022-06-20 17:52:26 +0100

    first commit

---
+++ calc.js
@@ -0,0 +1,18 @@
+let opr;
+let num1;
+let num2;
+
+if (opr == "+") {
+    num1 + num2
+} else if (opr == "-") {
+    num1 - num2
+} else if(opr == "/") {
+    num1 / num2 
+} else if(opr == "*") {
+    num1 * num2
+}
+
+    console.log(12 + 12);
+    console.log(12 - 2);
+    console.log(12 / 1);
+    console.log(12 * 12);
